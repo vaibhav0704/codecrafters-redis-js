2967be8b80488f95529d7e8bc37f69a366695c5c
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2023-08-10 12:30:01 +0530

    pass 2nd stage - responds to a PING request without parsing the data received

--- app/main.js
+++ app/main.js
@@ -6,6 +6,15 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
-});
+  connection.addListener('data', (data) => {
+    console.log(data.toString());
+
+    // response to an event where we get some data
+    connection.write('+PONG\r\n')
+    
+  })
+}).on('error', (err) => {
+  console.error(err);
+})
 //
 server.listen(6379, "127.0.0.1");
